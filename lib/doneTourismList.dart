03e08e7d9f79d05da5de63f2097ccf4eeff5c2d0
Arief2202 <47237543+Arief2202@users.noreply.github.com>
2022-03-23 05:29:55 +0700

    Tugas Praktikum 4

--- lib/doneTourismList.dart
+++ lib/doneTourismList.dart
@@ -23,9 +23,29 @@ class DoneTourismList extends StatelessWidget{
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text(
-                  place.title,
-                  style: const TextStyle(fontSize: 16.0),
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('assets/images/' + place.img),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          place.title,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(place.location),
+                      ],
+                    ),
+                  ),
                 ),
                 const Icon(Icons.done_outline),
               ],
